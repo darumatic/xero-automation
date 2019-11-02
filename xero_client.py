eaf4f21d8b388fecaeb9360b7898659925971328
Adrian Deccico <adrian@darumatic.com>
2019-11-02 17:22:44 +1100

    general refactoring

--- xero_client.py
+++ xero_client.py
@@ -1,4 +1,5 @@
 import json
+import time
 import urllib
 
 import requests
@@ -10,52 +11,67 @@ class XeroClient:
     def __init__(self, consumer_key, rsa_key):
         self.credentials = PrivateCredentials(consumer_key, rsa_key)
         self.headers = {'Accept': 'application/json'}
+        self.MAX_TRIES = 9
+        self.cache = {}
 
-    def get_all_items(self, url):
-        response = []
-        page = 1
-        appender = "&" if ("?" in url.split("/")[-1]) else "?"
+    def get_request(self, url):
+        response = None
+        tries = 0
         while True:
-            r = requests.get("{0}{1}page={2}&pageSize=500".format(url, appender, page), headers=self.headers, auth=self.credentials.oauth)
+            print("Getting request...  {0}".format(url))
+            if url in self.cache.keys():
+                print("Request is cached")
+                response = self.cache[url]
+                break
+            r = requests.get(url, headers=self.headers, auth=self.credentials.oauth)
             if r.status_code != 200:
                 error = r.text
-                raise RuntimeError('failed to load project, response=' + error)
-            r = r.json()
-            response.append(r)
-            if r["pagination"]["pageCount"] > page:
-                page = page + 1
+                too_many_requests = ("Max retries exceeded" in error) or ("rate%20limit%20exceeded" in error)
+                if self.MAX_TRIES > tries and too_many_requests:
+                    print("Max retries error. Sleeping 10 seconds before retrying...")
+                    time.sleep(10)
+                    tries = tries + 1
+                    continue
+                else:
+                    raise Exception('failed to get request, response=' + error)
             else:
+                response = r.json()
+                self.cache[url] = response
                 break
+        return response
+
+    def get_items(self, url, one_page=True):
+        response = None
+        if not one_page:
+            response = []
+            page = 1
+            appender = "&" if ("?" in url.split("/")[-1]) else "?"
+            while True:
+                url_rq = "{0}{1}page={2}&pageSize=500".format(url, appender, page)
+                r = self.get_request(url_rq)
+                response.append(r)
+                if r["pagination"]["pageCount"] > page:
+                    page = page + 1
+                else:
+                    break
+        else:
+            response = self.get_request(url)
         return response
 
     def project(self, project_id):
         url = 'https://api.xero.com/projects.xro/2.0/projects/' + project_id
-        headers = {'Accept': 'application/json'}
-        r = requests.get(url, headers=headers, auth=self.credentials.oauth)
-        if r.status_code != 200:
-            error = r.text
-            raise Exception('failed to load project, response=' + error)
-        return r.json()
+        return self.get_items(url)
 
     def get_projects(self):
         url = 'https://api.xero.com/projects.xro/2.0/projects'
-        return self.get_all_items(url)
+        return self.get_items(url)
 
     def get_active_projects(self):
-        return self.get_all_items('https://api.xero.com/projects.xro/2.0/projects?states=INPROGRESS')
-
-    def get(self, url):
-        return self.get_all_items(url)
+        return self.get_items('https://api.xero.com/projects.xro/2.0/projects?states=INPROGRESS')
 
     def task(self, project_id, task_id):
         url = 'https://api.xero.com/projects.xro/2.0/projects/' + project_id + '/tasks/' + task_id
-        headers = {'Accept': 'application/json'}
-        r = requests.get(url, headers=headers, auth=self.credentials.oauth)
-        if r.status_code != 200:
-            error = r.text
-            raise Exception('failed to load task, response=' + error)
-
-        return r.json()
+        return self.get_items(url, one_page=True)
 
     def time(self, project_id, start_time, end_time):
         url = 'https://api.xero.com/projects.xro/2.0/projects/' + project_id + '/time?'
@@ -64,28 +80,13 @@ class XeroClient:
         if end_time is not None:
             url += 'dateBeforeUtc=' + urllib.quote(self.to_json_timestamp(end_time))
 
-        headers = {'Accept': 'application/json'}
-        r = requests.get(url, headers=headers, auth=self.credentials.oauth)
-        print self.credentials.oauth_token
-        print r.status_code
-        print r.text
-        if r.status_code != 200:
-            print("XXXXXXXXXXXXXX",r.status_code)
-            error = r.text
-            raise Exception('failed to load project time, response=' + error)
-
-        body = r.json()
+        body = self.get_items(url)
         return body['items']
 
     def user(self, user_id):
         url = 'https://api.xero.com/projects.xro/2.0/projectsusers'
-        headers = {'Accept': 'application/json'}
-        r = requests.get(url, headers=headers, auth=self.credentials.oauth)
-        if r.status_code != 200:
-            error = r.text
-            raise Exception('failed to load user, response=' + error)
+        users = self.get_items(url, one_page=True)
 
-        users = r.json()
         for user in users['items']:
             if user['userId'] == user_id:
                 return user
@@ -93,13 +94,7 @@ class XeroClient:
 
     def contact(self, contact_id):
         url = 'https://api.xero.com/api.xro/2.0/Contacts/' + contact_id
-        headers = {'Accept': 'application/json'}
-        r = requests.get(url, headers=headers, auth=self.credentials.oauth)
-        if r.status_code != 200:
-            error = r.text
-            raise Exception('failed to load contact, response=' + error)
-
-        return r.json()
+        return self.get_items(url)
 
     def to_json_timestamp(self, utc_datetime):
         return json.dumps(utc_datetime.isoformat()).replace('"', '') + 'Z'
